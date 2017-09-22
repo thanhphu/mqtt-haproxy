6960dedc937192e375509f728e8d1738ded93b95
Thanh Phu <thanh.phu@daliworks.net>
2017-09-22 19:37:56 +0900

    Simplify bash, remove resident mode (causing problems), update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-FROM haproxy
+FROM haproxy:1.5
 LABEL maintainer="Thanh Phu <nvtphu@gmail.com>"
 
-VOLUME /certs:/certs
+# VOLUME /certs:/certs
 
 # Define working directory.
 WORKDIR /etc/haproxy
@@ -12,11 +12,10 @@ ADD restart.bash haproxy-restart
 # Add personalized configuration
 ADD haproxy.cfg haproxy.cfg
 
-# Define default command.
-CMD ["bash", "/haproxy-start"]
-# ENTRYPOINT [ "bash" ]
-
 # Expose ports.
 EXPOSE 8080
 # EXPOSE 443
 EXPOSE 1883
+
+# Define default command.
+CMD ["bash", "haproxy-restart"]
--- restart.bash
+++ restart.bash
@@ -1,28 +1,11 @@
 #!/bin/bash
 
 HAPROXY="/etc/haproxy"
-OVERRIDE="/haproxy-override"
 PIDFILE="/var/run/haproxy.pid"
-
-CONFIG="haproxy.cfg"
-ERRORS="errors"
+CONFIG="/etc/haproxy/haproxy.cfg"
 
 cd "$HAPROXY"
-
 pkill -9 -f haproxy
+haproxy -f $CONFIG -p "$PIDFILE"
 
-# Symlink errors directory
-if [[ -d "$OVERRIDE/$ERRORS" ]]; then
-  mkdir -p "$OVERRIDE/$ERRORS"
-  rm -fr "$ERRORS"
-  ln -s "$OVERRIDE/$ERRORS" "$ERRORS"
-fi
-
-# Symlink config file.
-if [[ -f "$OVERRIDE/$CONFIG" ]]; then
-  rm -f "$CONFIG"
-  ln -s "$OVERRIDE/$CONFIG" "$CONFIG"
-fi
-
-haproxy -f /etc/haproxy/haproxy.cfg -D -p "$PIDFILE"
 
